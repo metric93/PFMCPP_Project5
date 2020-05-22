5926ccd90cad667c4be4a26a27923473742877a8
matkatmusic <matkatmusic@gmail.com>
2020-05-22 15:49:51 -0700

    added part 4

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
 /*
- Project 5: Part 3 / 4
- video: Chapter 3 Part 4: 
+ Project 5: Part 4 / 4
+ video: Chapter 3 Part 7
 
-Create a branch named Part3
-
- the 'new' keyword
-
- 1) add #include "LeakedObjectDetector.h" to main (DONE)
+ Create a branch named Part4
  
- 3) Add 'JUCE_LEAK_DETECTOR(OwnerClass)' at the end of your UDTs.
+ Don't #include what you don't use
  
- 4) write the name of your class where it says "OwnerClass"
+ 1) Your task is to refactor your Chapter 3 Part 4 task into separate source and header files.
+         Add files via the pane on the left.
  
- 5) write wrapper classes for each type similar to how it was shown in the video
+ 2) Put all of your declarations for each class in .h files
+         One header file per class ( i.e. Raider.h for a class named "Raider" )
  
- 7) update main to use your wrapper classes, which have your UDTs as pointer member variables.
+ 3) Put all of your implementations in .cpp files.
+         one cpp file per class ( i.e. Raider.cpp for a class named "Raider" )
  
- 8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 4) Put all of your Wrapper classes in a single Wrappers.h file
+         if you implemented your wrapper class functions in-class, you'll need to move them to Wrappers.cpp
  
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
-
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
-
-send me a DM to check your pull request
-
- Wait for my code review.
+ 5) NO IN-CLASS IMPLEMENTATION ALLOWED.
+         the only exception is the existing Atomic.h and LeakedObjectDetector.h
  
- see here for an example: https://repl.it/@matkatmusic/ch3p04example
+ 6) for every .cpp file you have to make, insert it into the .replit file after 'main.cpp'.  Don't forget the spaces between file names.
+ If you need help with this step, send me a DM.
+ 
+ 7) click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+ Remember, your Chapter 3 Part 4 task worked when it was all in one file. so you shouldn't need to edit any of it.  
+         just split it up into source files and provide the appropriate #include directives.
  */
+
+
+
 
 
 
@@ -382,6 +386,20 @@ struct DawWrapper
         delete address;
     }
 };
+
+/*
+ MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
+
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ If you didn't already: 
+    Make a pull request after you make your first commit
+    pin the pull request link and this repl.it link to our DM thread in a single message.
+
+ send me a DM to review your pull request when the project is ready for review.
+
+ Wait for my code review.
+ */
 
 #include <iostream>
 int main()
